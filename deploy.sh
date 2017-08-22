4174ea3cdee35aba8613b351f67f8bb8931df6d9
SabineWren <sabinewrensc@gmail.com>
2017-08-22 06:48:37 -0700

    replaced button view-tabs with wings; replaced all button blue with orange; removed some browser-specific prefixes (breaking change -- will automate prefixing later)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html &&
 whoami 1>> /var/www/html/logs/deploy 2>&1 &&
-git reset --hard HEAD 1>/var/www/html/logs/deploy 2>&1 &&
+git reset --hard HEAD 1>> /var/www/html/logs/deploy 2>&1 &&
 git pull 1>> /var/www/html/logs/deploy 2>&1 &&
 chmod 775 deploy.php &&
 chmod 775 deploy.sh
