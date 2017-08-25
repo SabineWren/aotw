a734200b38c115e63bab23432aec4d222a89ffd6
SabineWren <sabinewrensc@gmail.com>
2017-08-25 08:24:27 -0700

    integrated menu into hud; optimized images; normalized most of the text; fixed a bug where the HUD would fall behind items or obscure links

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
+var gulp         = require('gulp');
+var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 
-	gulp.task('css', function () {
+gulp.task('css', function () {
 	var plugins = [
 		autoprefixer({browsers: ['> 1%'], cascade: false})
 	];
 	
-	return gulp.src('./proto/css-dev/*.css')
+	return gulp.src('./development/css/*.css')
 	.pipe(postcss(plugins))
-	.pipe(gulp.dest('./proto/css'));
+	.pipe(gulp.dest('./css'));
+});
+
+gulp.task('pages', function () {
+	return gulp.src('./development/pages/*')
+	.pipe(gulp.dest('./pages/'));
+});
+
+gulp.task('make', ['css', 'pages'], function () {
+	return gulp.src('./development/index.html')
+	.pipe(gulp.dest('./'));
 });
